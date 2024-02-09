a5466e62d189498ea01213c916f326127d6d7e32
Daniel Cook <87423173+DCFUKSURMOM@users.noreply.github.com>
2024-02-09 13:15:20 -0600

    modified freedos welcome.bat

---
+++ welcome.bat
@@ -0,0 +1,6 @@
+@ECHO off
+echo Welcome to the %OS_NAME% %OS_VERSION% operating system (http://www.freedos.org)
+echo This disk is set up with the tools to create an XTIDE boot floppy
+echo Please run xtidecfg to reconfigure the XTIDE BIOS first.
+echo The file you want to edit is ide_xt.bin
+echo When you are done configuring the XTIDE BIOS, run makeboot.bat
